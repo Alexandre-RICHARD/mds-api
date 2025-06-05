8022760ae114945fdd0e0feb11b978e89942786c
Alexandre RICHARD <arichard@predell.fr>
2025-06-05 18:21:15 +0200

    Fix types

--- src/v1/types/modelsCreationAttributes/u_user.sequelizeAttributes.type.ts
+++ src/v1/types/modelsCreationAttributes/u_user.sequelizeAttributes.type.ts
@@ -1,3 +1,13 @@
+import type { Optional } from "sequelize";
+
 import type { UserModelAttributes } from "../models/u_user.type";
 
-export type UserCreationAttributes = Omit<UserModelAttributes, "u_id_user">;
+export type UserCreationAttributes = Optional<
+  UserModelAttributes,
+  | "u_id_user"
+  | "u_adress_city"
+  | "u_adress_country"
+  | "u_adress_location"
+  | "u_adress_precision"
+  | "u_adress_region_code"
+>;
